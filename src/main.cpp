aa2f2d6bee4bf6c1eaa68a1b04fe5c3060fc027f
Curtis McEnroe <programble@gmail.com>
2011-08-03 16:55:19 -0400

    main should be noreturn

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-#include <types.h>
+#include <system.h>
 #include <multiboot.h>
 
 extern "C"
-void main(multiboot_header *multiboot, u32 magic)
+noreturn main(multiboot_header *multiboot, u32 magic)
 {
     *(u8*)0xB8000 = multiboot->bootloader_name[0];
+    reboot();
 }
