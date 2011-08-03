cf366c982c9422fcc02721fa51fa4c3fe06c553c
Curtis McEnroe <programble@gmail.com>
2011-08-03 16:27:56 -0400

    Added typedefs

---
+++ include/types.h
@@ -0,0 +1,17 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short u16;
+typedef signed short s16;
+typedef unsigned int u32;
+typedef signed int s32;
+typedef unsigned long long u64;
+typedef signed long long s64;
+
+#define string char*
+
+#define NULL ((void*) 0)
+
+#endif
