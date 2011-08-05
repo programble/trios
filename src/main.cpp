8bb3765eba20ca0a74c5c79d0e8c485bfc61bad2
Curtis McEnroe <programble@gmail.com>
2011-08-05 14:32:51 -0400

    Output bootloader name

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,14 @@ using namespace Devices::Output::Text;
 extern "C"
 noreturn main(multiboot_header *multiboot, u32 magic)
 {
+    assert(magic == MULTIBOOT_BOOTLOADER_MAGIC, "This bootloader is not magical enough!"); // No one will ever see this message anyway...
+
     Devices::Output::Text::Serial::init();
     Devices::Output::Text::Console::init();
+
+    print("booted with ");
+    print(multiboot->bootloader_name);
+    print("\n");
+
     panic("the kernel is a lie!");
 }
